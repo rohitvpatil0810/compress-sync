4b5080041f60cc4b8a6ef39ce838457a888caaf8
Rohit Patil <rohitvpatil0810@gmail.com>
2025-03-09 13:51:25 +0530

    feat: add webhook simulation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { SuccessResponse } from "../core/ApiResponse";
+import { InternalErrorResponse, SuccessResponse } from "../core/ApiResponse";
 import imageRouter from "./imageRouter";
 import csvProcesserRouter from "./csvProcesserRouter";
+import Logger from "../core/Logger";
 
 const router = Router();
 
@@ -15,6 +16,24 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
         "CompressSync is an asynchronous image processing system that extracts image data from CSV files, compresses images by 50%, and stores the processed results. It supports API-based status tracking and webhook integration for seamless automation.",
     };
     return new SuccessResponse("Welcome to Compress Sync API", data).send(res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/webhook", (req: Request, res: Response, next: NextFunction) => {
+  try {
+    Logger.info("Webhook Body", req.body);
+
+    // Simulate a failure in 50% of requests
+    if (Math.random() > 0.5) {
+      Logger.info("❌ Simulating failure...");
+      return new InternalErrorResponse("Simulated failure").send(res);
+    }
+
+    return new SuccessResponse("Webhook processed successfully", {
+      data: req.body,
+    }).send(res);
   } catch (error) {
     next(error);
   }
